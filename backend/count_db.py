91d8b30ffa5945bbb1a37b4eb4ae4d4048aacdcc
TGAshwinYT <ashwinrm2005@gmail.com>
2026-03-13 00:38:53 +0530

    chore: cleanup unused legacy files and diagnostic scripts

--- backend/count_db.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-from sqlalchemy import text
-sys.path.insert(0, os.getcwd())
-from app.database import MandiSessionLocal
-db = MandiSessionLocal()
-for d in ['27/02/2026', '26/02/2026', '25/02/2026', '24/02/2026', '23/02/2026']:
-    print(f"{d}:", db.execute(text(f"SELECT count(*) FROM mandi_rates WHERE arrival_date = '{d}'")).scalar())
-db.close()
